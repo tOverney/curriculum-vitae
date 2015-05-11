8a31350fa896eb1657b51b656aa105d30f469f64
Damien Engels <engelsdamien@gmail.com>
2015-05-11 17:39:07 +0200

    layureo package

--- install_tex_packages.sh
+++ install_tex_packages.sh
@@ -3,4 +3,11 @@
 # Font Awesome
 wget http://www.pirbot.com/mirrors/ctan/fonts/fontawesome/fontawesome.sty
 
+# Layaureo
+wget http://mirrors.ctan.org/macros/latex/contrib/layaureo.zip
+unzip layaureo.zip
+cd layaureo/SourceFiles
+latex layaureo.ins
+mv layaureo.sty ../..
+cd ../..
 
