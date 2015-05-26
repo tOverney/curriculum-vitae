d2b419dd778b0726ea654994a6589aee43308251
Valérian Rousset <valerian.rousset@epfl.ch>
2015-05-26 21:00:59 +0200

    Some shell improvements

--- install_tex_packages.sh
+++ install_tex_packages.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
 # Font Awesome
 wget http://www.pirbot.com/mirrors/ctan/fonts/fontawesome/fontawesome.sty
--- update_cv.sh
+++ update_cv.sh
@@ -1,33 +1,34 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
 set -x
 
-REPO="curriculum-vitae"
+readonly REPO="curriculum-vitae"
+
+readonly SRC="build/cv.pdf"
+readonly TARGET="dengels.ch/public/doc/cv.pdf"
 
 if [ "$TRAVIS_REPO_SLUG" == "paullepoulpe/$REPO" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
-    
-    # Setup travis info 
+
+    # Setup travis info
     git config --global user.email "travis@travis-ci.org"
     git config --global user.name "travis-ci"
 
-    # Clone website repository 
+    # Clone website repository
     git clone --quiet --branch=master https://${GH_TOKEN}@github.com/paullepoulpe/dengels.ch
-     
+
     # Clear old file
-    SRC=build/cv.pdf
-    TARGET=dengels.ch/public/doc/cv.pdf
-    rm -rf $TARGET
-    
-    # Copy new file 
-    cp -v $SRC $TARGET
-    
+    rm -rf "$TARGET"
+
+    # Copy new file
+    cp -v "$SRC" "$TARGET"
+
     # Step into repo
-    cd dengels.ch/
-    
+    cd "${TARGET%%/*}"
+
     # Mark for add and commit
-    git add public/doc/cv.pdf
+    git add "${TARGET#*/}"
     git commit -m "Travis #$TRAVIS_BUILD_NUMBER: Update resume commit $TRAVIS_COMMIT"
-    git push origin master 
+    git push origin master
 else
     echo "Conditions not met to update resume"
 fi
