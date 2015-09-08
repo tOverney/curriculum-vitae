eaec4dfd49657a64f908902ef121113ab148a42a
Damien Engels <engelsdamien@gmail.com>
2015-09-07 19:29:06 -0700

    Fixed tex package install

--- install_tex_packages.sh
+++ install_tex_packages.sh
@@ -6,6 +6,7 @@ set -e // Exit on error
 # Font Awesome
 wget http://mirrors.ctan.org/fonts/fontawesome/tex/fontawesome.sty
 wget http://mirrors.ctan.org/fonts/fontawesome/tex/fontawesomesymbols-generic.tex
+wget http://mirrors.ctan.org/fonts/fontawesome/tex/fontawesomesymbols-xeluatex.tex
 
 # Layaureo
 wget http://mirrors.ctan.org/macros/latex/contrib/layaureo.zip
