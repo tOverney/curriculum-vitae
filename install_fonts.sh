91e699f9a6ef0286c403cdcdad1c8a17e0df9939
Damien Engels <engelsdamien@gmail.com>
2015-09-07 19:04:32 -0700

    Fixed install fonts script

--- install_fonts.sh
+++ install_fonts.sh
@@ -9,5 +9,5 @@ wget http://www.exljbris.com/dl/fontin_pc.zip
 unzip fontin_pc.zip -d ~/.fonts
 
 # Font awesome
-wget http://mirrors.ctan.org/fonts/fontawesome/FontAwesome.otf
-mv FontAwesome.otf ~/.fonts
+wget http://fontawesome.io/assets/font-awesome-4.4.0.zip -O font-awesome.zip
+unzip -j font-awesome.zip */fonts/* -d ~/.fonts
